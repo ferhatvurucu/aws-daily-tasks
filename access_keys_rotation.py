250040149f1db810257bf02012d5899b3c4ba612
Ferhat Vurucu <ferhatvrcu@gmail.com>
2020-09-18 17:18:22 +0300

    naming convention

--- access_keys_rotation.py
+++ access_keys_rotation.py
@@ -6,41 +6,41 @@ import datetime
 iam = boto3.client('iam')
 
 # define current date
-currentDate = datetime.date.today()
+current_date = datetime.date.today()
 
 def create_user_list():
-    listOfUsers = []
+    list_of_users = []
     users = iam.list_users()
     for user in users['Users']:
-        listOfUsers.append(user['UserName'])    
-    return listOfUsers
+        list_of_users.append(user['UserName'])    
+    return list_of_users
 
-def deactive_old_key(userName,accessKey):
-    iam.update_access_key(AccessKeyId=accessKey,Status='Inactive',UserName=userName)
-    print('Successfully deactived key for the user ' + userName)
+def deactive_old_key(username,accesskey):
+    iam.update_access_key(AccessKeyId=accesskey,Status='Inactive',UserName=username)
+    print('Successfully deactived key for the user ' + username)
 
-def create_new_key(userName):
+def create_new_key(username):
     try:
-        response=iam.create_access_key(UserName=userName)
-        accessKey=response['AccessKey']
-        print('Username: ' + userName + '\nAccessKey: ' + str(accessKey['AccessKeyId']) + '\nSecretAccessKey: ' + str(accessKey['SecretAccessKey']))
+        response=iam.create_access_key(UserName=username)
+        accesskey=response['AccessKey']
+        print('Username: ' + username + '\nAccessKey: ' + str(accesskey['AccessKeyId']) + '\nSecretAccessKey: ' + str(accesskey['SecretAccessKey']))
     except ClientError as e:
         print(e)
 
-def rotate_keys_for_users(listOfUsers):
-    for user in listOfUsers:
+def rotate_keys_for_users(list_of_users):
+    for user in list_of_users:
         keys = iam.list_access_keys(UserName=user)
         for key in keys['AccessKeyMetadata']:
-            creationDate=key['CreateDate']
-            accessKey=key['AccessKeyId']
-            keyStatus=key['Status']
-            if (currentDate - creationDate.date()).days > 90 and keyStatus == 'Active':
-                deactive_old_key(user,accessKey)
+            creation_date=key['CreateDate']
+            accesskey=key['AccessKeyId']
+            keystatus=key['Status']
+            if (current_date - creation_date.date()).days > 90 and keystatus == 'Active':
+                deactive_old_key(user,accesskey)
                 create_new_key(user)
-            elif keyStatus == 'Inactive':
+            elif keystatus == 'Inactive':
                 print(user + ' access key is inactive.')
             else:
                 print(user + ' access key is active for less then 90 days.')
 
-userList = create_user_list()
-rotate_keys_for_users(userList)
+user_list = create_user_list()
+rotate_keys_for_users(user_list)
--- ec2_start_stop.py
+++ ec2_start_stop.py
@@ -9,36 +9,36 @@ args = parser.parse_args()
 action=(args.action).upper()
 
 # create EC2 resource and client
-ec2Resource = boto3.resource('ec2')
-ec2Client = boto3.client('ec2')
+ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
 
 def create_ec2_list():
-    listOfInstances=[]
-    for instance in ec2Resource.instances.all():
-        listOfInstances.append(str(instance.id))
-        return listOfInstances
+    list_of_instances=[]
+    for instance in ec2_resource.instances.all():
+        list_of_instances.append(str(instance.id))
+        return list_of_instances
 
-def start_instances(instanceList):
+def start_instances(instances):
     try:
-        for id in instanceList:
-            ec2Client.start_instances(InstanceIds=[id], DryRun=False)
+        for id in instances:
+            ec2_client.start_instances(InstanceIds=[id], DryRun=False)
             print(id + ' has been started.')
     except ClientError as e:
         print(e)
 
-def stop_instances(instanceList):
+def stop_instances(instances):
     try:
-        for id in instanceList:
-            ec2Client.stop_instances(InstanceIds=[id], DryRun=False)
+        for id in instances:
+            ec2_client.stop_instances(InstanceIds=[id], DryRun=False)
             print(id + ' has been stopped.')
     except ClientError as e:
         print(e)
 
-instanceList = create_ec2_list()
+instance_list = create_ec2_list()
 
 if action == 'START':
-    start_instances(instanceList)
+    start_instances(instance_list)
 elif action == 'STOP':
-    stop_instances(instanceList)
+    stop_instances(instance_list)
 else:
     print('usage: ec2_start_stop.py [-h] action')

8ea3412faee4dba270015bdc6aaef23e964e286a
Ferhat Vurucu <ferhatvrcu@gmail.com>
2020-09-24 12:37:35 +0300

    rds manual snapshots

---
+++ scripts/rds_manual_snapshots.py
@@ -0,0 +1,19 @@
+import boto3
+
+# create RDS client
+rds = boto3.client('rds')
+
+def create_rds_list():
+    list_of_dbs = []
+    dbs = rds.describe_db_instances()['DBInstances']
+    for db in dbs:
+        list_of_dbs.append(db['DBInstanceIdentifier'])
+    return list_of_dbs
+
+def create_rds_snapshot(dbs):
+    for db in dbs:
+        rds.create_db_snapshot(DBSnapshotIdentifier=db+'-snapshot', DBInstanceIdentifier=db)
+        print("Creating snapshot for " + db)
+
+dbs = create_rds_list()
+create_rds_snapshot(dbs)
